dc539376a921dcbc4f064cb01b927912ca5b49df
lordspinach <ksgamayunov@gmail.com>
2020-01-30 22:26:10 +0300

    My homework. Added heroes stats

--- ConsoleApp1/hero.cs
+++ ConsoleApp1/hero.cs
@@ -7,6 +7,17 @@ namespace ConsoleApp1
     public class hero
     {
         private string _name;
+        private string _class;
+        private int _health;
+        private int _mana;
+
+        public hero()
+        {
+            _name = "";
+            _class = "";
+            _health = -1;
+            _mana = -1;
+        } 
 
         public string Name
         {
@@ -20,6 +31,47 @@ namespace ConsoleApp1
                 _name = value;
             }
         }
+
+        public string Class
+        {
+            get
+            {
+                return _class;
+            }
+
+            set
+            {
+                _class = value;
+            }
+        }
+
+        public int Health
+        {
+            get
+            {
+                return _health;
+            }
+
+            set
+            {
+                _health = value;
+            } 
+        }
+
+        public int Mana
+        {
+            get
+            {
+                return _mana;
+            }
+
+            set
+            {
+                _mana = value;
+            }
+        }
+
+        
 
         public virtual string FirstSkill()
         {
@@ -40,5 +92,7 @@ namespace ConsoleApp1
         {
 
         }
+
+        
     }
 }
